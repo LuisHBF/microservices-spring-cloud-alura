e7259d8f6f5c90ea4d27c29bb92fbcbedea64652
Luis Henrique B. Franco <luishbfranco@gmail.com>
2021-06-20 03:49:35 -0300

    configurando microservice de fornecedor para recuperar as informações do BD diretamente do config-server

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FornecedorApplication {
 
 	public static void main(String[] args) {
+        System.setProperty("spring.cloud.bootstrap.enabled","true");
 		SpringApplication.run(FornecedorApplication.class, args);
 	}
 
