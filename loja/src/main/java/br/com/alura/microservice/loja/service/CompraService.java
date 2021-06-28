068867bfa58b7c00154d365d8daeabf7f132114c
Luis Henrique B. Franco <luishbfranco@gmail.com>
2021-06-28 02:40:14 -0300

    Adicionando Feign para comunicação entre serviços

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,27 +1,22 @@
 package br.com.alura.microservice.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
-
-
+	
 	@Autowired
-	private RestTemplate client;
+	private FornecedorClient fornecedorClient;
 	
 	public void realizaCompra(CompraDTO compra) {
-
-		String estado = compra.getEndereco().getEstado();
-		ResponseEntity<InfoFornecedorDTO> exchange = this.client.exchange("http://fornecedor/info/" + estado, HttpMethod.GET, null, InfoFornecedorDTO.class);
-		System.out.println(exchange.getBody().getEndereco());
+		InfoFornecedorDTO info = this.fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 		
+		System.out.println(info.getEndereco());
 	}
 
 }
